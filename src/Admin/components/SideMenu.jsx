bf593595ef8653f6b33dbeaed3f69852f8f4b28f
ridha <ridhamariyam@gmail.com>
2023-12-29 18:45:39 +0530

    privateroute issue fixedd

--- src/Admin/components/SideMenu.jsx
+++ src/Admin/components/SideMenu.jsx
@@ -76,7 +76,7 @@ const SideMenu = () => {
                 </li>
               </ul>
               <Link
-                to={"#"}
+                to={"/DoctorList"}
                 className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group "
               >
                 <svg
