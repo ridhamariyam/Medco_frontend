9263b2eeab7422d3377ea525cab4d2d582f8894c
ridha <ridhamariyam@gmail.com>
2023-12-29 10:43:33 +0530

    urls changed

--- src/const/urls.js
+++ src/const/urls.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://127.0.0.1:8000'
+export const baseUrl = 'https://meddic.website'
 export const wsUrl = process.env.REACT_APP_WS_URL;
 console.log(process.env.REACT_APP_BASE_URL, 'dff')
